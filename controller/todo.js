f27c24b140b0aac82c65d63fed8b0969cec06364
oneraner <oneraner@gmail.com>
2022-05-17 09:29:12 +0800

    completed api

--- controller/todo.js
+++ controller/todo.js
@@ -1,73 +1,74 @@
 // Model
-const Todo = require('../model/todo.js')
-const ApiState = require('../utils/apiState.js')
-const AppError = require('../utils/appError.js')
+const Todo = require("../model/todo.js");
+const ApiState = require("../utils/apiState.js");
+const AppError = require("../utils/appError.js");
 // Utils
-const catchAsync = require('../utils/catchAsync')
-const { successHandle } = require('../utils/resHandle.js')
+const catchAsync = require("../utils/catchAsync");
+const { successHandle } = require("../utils/resHandle.js");
 
 // 取得待辦列表 GET /todos
 const getTodo = catchAsync(async (req, res) => {
   res.json({
-    "todos": [
+    todos: [
       {
-        "id": "6990914bd981fad6be4d587f024f7c0b",
-        "content": "123",
-        "completed_at": "2022-05-13T16:12:16.615+08:00"
-      }
-    ]
-  }
-  )
-  // error 
+        id: "6990914bd981fad6be4d587f024f7c0b",
+        content: "123",
+        completed_at: "2022-05-13T16:12:16.615+08:00",
+      },
+    ],
+  });
+  // error
   // {
   //   "message": "未授權"
   // }
-  
-})
+});
 
 // 新增待辦  POST /todos
 const createTodo = catchAsync(async (req, res, next) => {
   res.json({
-    "id": "7ee245b09ff5b738b01f83ee5dabadfa",
-    "content": "string"
-  })
-  // error 
+    id: "7ee245b09ff5b738b01f83ee5dabadfa",
+    content: "string",
+  });
+  // error
   // {
   //   "message": "未授權"
   // }
-})
+});
 
 // 編輯待辦  PUT /todos/:todo_id
 const editTodo = catchAsync(async (req, res, next) => {
   res.json({
-      "id": "7ee245b09ff5b738b01f83ee5dabadfa",
-      "content": "string"
-    })
-})
+    id: "7ee245b09ff5b738b01f83ee5dabadfa",
+    content: "string",
+  });
+});
 
 // 刪除待辦  DELETE /todos/:todo_id
 const deleteTodo = catchAsync(async (req, res, next) => {
   res.json({
-    "message": "已刪除"
-  }
-  )
-})
+    message: "已刪除",
+  });
+});
 
 // 切換待辦完成狀態  PATCH /todos/:todo_id/toggle
 const toggleTodo = catchAsync(async (req, res, next) => {
-  res.json({
-    "id": "ec7ebef6fa77ed77cc7392439231c4b8",
-    "content": "string",
-    "completed_at": "2022-05-13T17:21:18.856+08:00"
-  }
-  )
-})
-
+  const { todo_id } = req.params;
+  const todoData = await Todo.find({ _id: todo_id });
+  const currentCompletedAt = !!todoData[0].completedAt;
+  const updateCompletedAt = {
+    completedAt: currentCompletedAt ? null : Date.now(),
+  };
+  const updateTodo = await Todo.findByIdAndUpdate(todo_id, updateCompletedAt, {
+    new: true,
+    runValidators: true,
+  });
+  res.json(updateTodo);
+});
 
 module.exports = {
   getTodo,
   createTodo,
   editTodo,
   deleteTodo,
-  toggleTodo
-}
+  toggleTodo,
+};
--- model/todo.js
+++ model/todo.js
@@ -1,26 +1,31 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const TodoSchema = new mongoose.Schema(
   {
     // 發文者名稱
     user_id: {
       type: String,
-      required: [true, '登入者id為必填'],
+      required: [true, "登入者id為必填"],
     },
     // 發文內容
     content: {
       type: String,
-      required: [true, '內容為必填'],
+      required: [true, "內容為必填"],
     },
     // 完成時間，轉為 Timestamp 以方便前端好處理
     createdAt: {
       type: Number,
       default: new Date().getTime(),
     },
+    // 已完成狀態
+    completedAt: {
+      type: Number,
+      default: null,
+    },
   },
-  { versionKey: false },
-)
+  { versionKey: false }
+);
 
-const Todo = mongoose.model('Todo', TodoSchema)
+const Todo = mongoose.model("Todo", TodoSchema);
 
-module.exports = Todo
+module.exports = Todo;
