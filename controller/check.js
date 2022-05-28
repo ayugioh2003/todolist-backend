c90c2ceb637389b621da6512448d84a88959187c
ming wu <a85062@pchome.com.tw>
2022-05-28 21:09:55 +0800

    【新增】check路由

--- controller/check.js
+++ controller/check.js
@@ -5,7 +5,7 @@ const catchAsync = require('../utils/catchAsync')
 const AppError = require('../utils/appError')
 const { successHandle } = require('../utils/resHandle.js')
 const ApiState = require('../utils/apiState')
-
+const { verifyToken } = require('../utils/verification')
 /*
   res 回傳錯誤範例
   return next(new AppError(ApiState.FIELD_MISSING))
@@ -13,24 +13,44 @@ const ApiState = require('../utils/apiState')
   ApiState.js 可自行新增需要的錯誤內容
 */
 
-
 /*
   登入功能	GET	/check
 */
 const check = catchAsync(async (req, res, next) => {
-  res.json({
-    "message": "OK!"
-  })
+  // 確認 token 是否存在
+  let token
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    [, token] = req.headers.authorization.split(' ')
+  }
+  if (!token) {
+    return next(
+      new AppError({
+        message: 'token不存在',
+        status: ApiState.DATA_EXIST.status,
+        statusCode: ApiState.DATA_EXIST.statusCode,
+      })
+    )
+  }
 
-  // error
-  // {
-  //   "message": "未授權"
-  // }
-  
+  // 取的token驗證通過解密出來的使用者id
+  const verify = await verifyToken(token)
+  if (verify) {
+    const result = await User.findOne({ _id: verify }, '_id name email')
+    return successHandle({ res, message: '驗證通過', data: result })
+  }
+
+  return next(
+    new AppError({
+      message: '未授權',
+      status: ApiState.FAIL.status,
+      statusCode: ApiState.FAIL.statusCode,
+    })
+  )
 })
 
-
-
 module.exports = {
-  check
+  check,
 }
